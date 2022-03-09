8ec91545e8e782e5ee864460b44458d64f2a85a2
97688 <97688@DESKTOP-1TH7HQE>
2022-03-09 18:33:43 +0800

    Hi first lab1

---
+++ src/List2.java
@@ -0,0 +1,12 @@
+
+public interface List2 {
+	
+	   public boolean isEmpty();
+	   public int size();
+	   public Object get(int index);
+	   public int indexOf(Object theElement);
+	   public Object remove(int index);
+	   public void add(int index, Object theElement);
+	   public String toString();
+	}
+
